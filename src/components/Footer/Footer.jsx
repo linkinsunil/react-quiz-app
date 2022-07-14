089be5aca60228fea6f4b607f55aeaa7887daae1
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-07-14 17:05:21 +0530

    feat: footer component added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      Made with ❤️ by{' '}
+      <a href='https://www.linkedin.com/in/linkinsunil/'>Sunil</a>
+    </div>
+  );
+}
+
+export default Footer;
