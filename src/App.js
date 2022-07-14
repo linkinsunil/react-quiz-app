9fe5c69302d8d7a31d36c04265a84c168a4ab83c
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-07-14 17:06:46 +0530

    fix: binded pages in routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Quiz from './pages/Quiz/Quiz';
+import Result from './pages/Result/Result';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app'>
-      <h1>React App</h1>
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <Header />
+
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/quiz' element={<Quiz />} />
+          <Route exact path='/result' element={<Result />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
