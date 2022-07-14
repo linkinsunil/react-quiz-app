7547ccdb4dcc6efde11ce7c6edcbe18a1965e8fa
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-07-14 17:05:57 +0530

    feat: home, quiz, result page added

---
+++ src/pages/Quiz/Quiz.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Quiz.css';
+
+function Quiz() {
+  return <div>Quiz</div>;
+}
+
+export default Quiz;
---
+++ src/pages/Result/Result.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Result.css';
+
+function Result() {
+  return <div>Result</div>;
+}
+
+export default Result;
