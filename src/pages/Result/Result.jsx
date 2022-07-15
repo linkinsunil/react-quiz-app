8ff6fe58f6fd2317cec33a976f81675087733802
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-07-16 00:00:33 +0530

    result page completed

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import './Result.css';
 
-function Result() {
-  return <div>Result</div>;
+function Result({ name, score }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!name) {
+      navigate('/');
+    }
+  }, [name, navigate]);
+
+  return (
+    <div className='result'>
+      <span className='title'>Final Score : {score}</span>
+      <Button
+        variant='contained'
+        color='secondary'
+        size='large'
+        style={{ alignSelf: 'center', marginTop: 20 }}
+        href='/'
+      >
+        Go To Homepage
+      </Button>
+    </div>
+  );
 }
 
 export default Result;
